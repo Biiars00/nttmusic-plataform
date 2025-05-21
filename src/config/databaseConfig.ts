65314825e3acbe6313caa0c1f89c84f6428e7da4
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-05-20 21:30:40 -0300

    feat: database config.

---
+++ src/config/databaseConfig.ts
@@ -0,0 +1,8 @@
+import admin from "firebase-admin";
+import "dotenv/config";
+
+const databaseConfig = admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(process.env.URL_FIREBASE_CREDENTIALS || "")),
+});
+
+export default databaseConfig;
